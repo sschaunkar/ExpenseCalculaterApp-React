8d006769a66ba86bda8edaf7112d7dcceab99920
sschaunkar <chhokar.sachin76@gmail.com>
2021-04-16 13:45:26 +0530

    small-bug-fixed

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -55,7 +55,7 @@ function ExpenseForm(props) {
         event.preventDefault();
         const expenseData = {
             title:title,
-            amount:amount,
+            amount:+amount,
             date:new Date(date)
         }
         props.onSaveExpenseData(expenseData);
